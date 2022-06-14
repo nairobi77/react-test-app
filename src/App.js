f1a71bbf7a285262c11905fd48e4bb981b322b00
Aleksandr Kurakin <a.kurakin@cian.ru>
2022-06-14 17:12:23 +0300

    added counter

--- src/App.js
+++ src/App.js
@@ -6,27 +6,26 @@ import {NumberLists} from "./components/NumberList";
 import {NameForm} from "./components/NameForm";
 import {EssayForm} from "./components/EssayForm";
 import {FlavorForm} from "./components/FlavorForm";
+import Counter from "./components/Counter";
 
 function App() {
-  const numbers = [1, 2, 3, 4, 5, 6]
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Clock/>
-        <div>
-          <NameForm/>
+    const numbers = [1, 2, 3, 4, 5, 6]
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Counter/>
+                <Counter/>
+                <Clock/>
+                <div>
+                    <NameForm/>
+                </div>
+                <LoginControl/>
+                <EssayForm/>
+                <FlavorForm/>
+                <NumberLists numbers={numbers}/>
+            </header>
         </div>
-        <LoginControl/>
-        <EssayForm/>
-        <FlavorForm/>
-        <NumberLists numbers={numbers}/>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+const Counter = () => {
+
+    const [count, setCount] = useState(0)
+
+    function incrementCount() {
+        setCount(count + 1)
+    }
+
+    function decrementCount() {
+        setCount(count - 1)
+    }
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={incrementCount}>Increment</button>
+            <button onClick={decrementCount}>Decrement</button>
+        </div>
+    );
+};
+
+export default Counter;
